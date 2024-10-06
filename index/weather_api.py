984f508e3deff91cb31dc07c910dc9b8fb849ad5
LestathRiveraD <cesarrivera0404@gmail.com>
2024-10-06 03:27:15 -0700

    full implementation of weather api

--- index/weather_api.py
+++ index/weather_api.py
@@ -4,11 +4,9 @@ api_key = 'b05ad82a832f5da790b48645e5943fc7'
 
 # From the name of a city, returns a tuple with its latitud and longitud respectively
 def lat_long(name):
-    url = f'http://api.openweathermap.org/geo/1.0/direct?q={name}&appid={api_key}'
-    response = requests.get(url).json()[0]
-    lat = response['lat']
-    long = response['lon']
-    return (lat, long)
+    url = f'http://api.openweathermap.org/geo/1.0/direct?q={name}&appid={api_key}&limit=100'
+    response = requests.get(url).json()
+    return response
 
 
 # Gathers current climate data from a city taking its latitud and longitud as input
@@ -20,6 +18,8 @@ def gather_data_latlong(lat, long):
 
 # Gathers current climate data from a city taking its name as input. Returb
 def gather_data_name(name):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={name}&appid={api_key}"
-    response = requests.get(url).json()
+    cities = lat_long(name)
+    response = []
+    for city in cities:
+        response.append(gather_data_latlong(city['lat'], city['lon']))
     return response
