6f9c55ded4a7a4b79f74debc24da0726575014bf
LestathRiveraD <cesarrivera0404@gmail.com>
2024-10-06 14:08:19 -0700

    Results page

--- index/app.py
+++ index/app.py
@@ -17,6 +17,28 @@ def search():
 
 @app.route("/results")
 def results():
-    data = request.args.get("data")
-    print(data)
-    return render_template("results.html", data=data)
+    # Retrieve the city weather data from the form
+    name = request.args.get("name")
+    country = request.args.get("country")
+    state = request.args.get("state")
+    temp = float(request.args.get("temp")) - 275.15
+    temp = "{:.2f}".format(temp)
+    humidity = request.args.get("humidity")
+    weather = request.args.get("weather")
+    wind_speed = request.args.get("wind_speed")
+    pressure = request.args.get("pressure")
+    visibility = request.args.get("visibility")
+    
+    # Pass the retrieved data to the template
+    return render_template(
+        "results.html", 
+        name=name, 
+        country=country, 
+        state=state, 
+        temp=temp, 
+        humidity=humidity, 
+        weather=weather,
+        wind_speed=wind_speed,
+        pressure=pressure,
+        visibility=visibility
+    )
