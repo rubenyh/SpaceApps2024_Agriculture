22703fb60263b3775021139adfec78ccaf3652b9
BigJuicyBear <enzoparlangem@gmail.com>
2024-10-06 02:03:11 -0700

    Le cambie todo

--- index/app.py
+++ index/app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')  # Get the search query from the URL
+    if query:
+        # Do something with the search query, for example:
+        return render_template('search_results.html', query=query)
+    else:
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
